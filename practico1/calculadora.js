03472862d1f77decb4c3aa03f595212febd006c4
Fernando-I-005 <fernandoiglesias05@gmail.com>
2023-08-11 22:00:31 -0300

    version acomodada y limpia

--- practico1/calculadora.js
+++ practico1/calculadora.js
@@ -35,6 +35,7 @@ function calculadora(a, b, c) {
 
 
 // clickea y visualiza todos los efectos
+//asignacion de valor de variables desde html
 document.getElementById("boton").addEventListener("click", function () {
     let a = parseFloat(document.getElementById("numero1").value);
     let b = parseFloat(document.getElementById("numero2").value);
@@ -43,43 +44,34 @@ document.getElementById("boton").addEventListener("click", function () {
     // validacion de datos ingresados
 
     if (isNaN(a) || isNaN(b)) {
-        document.getElementById("intrucciones").textContent = "Valor numerico ingresado inválido";
+        document.getElementById("intrucciones").textContent = "Valor numérico ingresado inválido";
         document.getElementById("parrafo").textContent = "...esperando para procesar";
     } else if (c == "division" && b == 0) {
-        document.getElementById("intrucciones").textContent = "Operacion inválida, no se puede dividir por cero"
+        document.getElementById("intrucciones").textContent = "Operación inválida, no se puede dividir por cero"
         document.getElementById("parrafo").textContent = "...esperando para procesar";
     }
-
-
     else {
         (calculadora(a, b, c));
 
-
-
         // validacion del resultado
-
-
         if (isNaN(resultado)) {   //funcion isnan() evalua si el resultado no es un nº - validacion de ingreso de simbolos como valores
-            document.getElementById("intrucciones").textContent = "Resultado  inválido, ingrese un valor correcto";
+            document.getElementById("intrucciones").textContent = "Resultado inválido, ingrese un valor correcto";
             document.getElementById("parrafo").textContent = "...esperando para procesar";
         } else if (Math.abs(resultado) >= 99999999999) {
-            document.getElementById("intrucciones").textContent = "El resultado final es muy grande, es mayor a cien mil millones!"; // se limita arbitrriamente a 11 digitos
-            document.getElementById("parrafo").textContent = "Limite de 11 digitos enteros superado";
-        } else if ((Math.abs(resultado) <= 0.0000001) && (Math.abs(resultado) != 0)) {  // validacion inferior excluyendo al cero
-            document.getElementById("intrucciones").textContent = "El resultado es muy pequeño, es menor a 0.0000001!";  //.toLocaleString()//.toFixed(10) // abs valida tando positicos como negativos
-            document.getElementById("parrafo").textContent = "Limite de 7 digitos decimales superado";
+            document.getElementById("intrucciones").textContent = "El resultado es muy grande, es mayor a cien mil millones!"; // se limita arbitrariamente a 11 digitos
+            document.getElementById("parrafo").textContent = "Límite de 11 dígitos enteros superado";
+        } else if ((Math.abs(resultado) <= 0.0000001) && (Math.abs(resultado) != 0)) {  // validacion inferior de nº absolutos menor a 0.0000001 excluyendo al cero
+            document.getElementById("intrucciones").textContent = "El valor del resultado es muy pequeño";  // .abs valida tando positivos como negativos
+            document.getElementById("parrafo").textContent = "se desprecian los resultados menores a 0.0000001";
         } else if ((Math.abs(resultado) >= 1) || (resultado == 0)) {
-            document.getElementById("parrafo").textContent = "El resultado es: " + resultado.toLocaleString() + "  ";    //.toLocaleString() lo pasa a string y lo muestra con puntuciones de miles pero redondea los decimales mal
-            document.getElementById("intrucciones").textContent = "¡¡OPERACIÓN EXITOSA >1!!!";                      /// resultado para odulo mayor a 1
+            document.getElementById("parrafo").textContent = "El resultado es: " + resultado.toLocaleString() ;    //.toLocaleString() lo pasa a string y lo muestra con puntuciones de miles pero redondea los decimales mal
+            document.getElementById("intrucciones").textContent = "¡¡OPERACIÓN EXITOSA!!";                      /// resultado para valores absolutos mayores a 1
         } else {
-            document.getElementById("parrafo").textContent = "El resultado es: " + resultado;    //.toLocaleString() lo pasa a string y lo muestra con puntuciones de miles pero redondea los decimales mal
-            document.getElementById("intrucciones").textContent = "¡¡OPERACIÓN EXITOSA<1!!!";                            /// modulo menor a 1 sin .toLocaleString() porque redonde mal
+            document.getElementById("parrafo").textContent = "El resultado es: " + resultado;    
+            document.getElementById("intrucciones").textContent = "¡¡OPERACIÓN EXITOSA!!";                            /// modulo menor a 1 sin .toLocaleString() porque redonde mal
         }
     }
 });
-
-
-
 
 //resetea y limpia formulario
 document.getElementById("reset").addEventListener("click", function () {
